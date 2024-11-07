ccf04088e16d761d0f4407d6d8bb96fdf34440a4
baptiste.baverel <baptiste.baverel4@gmail.com>
2024-11-07 14:50:05 +0100

    correction issues

--- src/test/java/liste/ListeSimpleTest.java
+++ src/test/java/liste/ListeSimpleTest.java
@@ -117,9 +117,8 @@ class ListeSimpleTest {
         listeATester.ajout(1);
         listeATester.ajout(2);
         listeATester.ajout(3);
-        listeATester.ajout(4);
         listeATester.supprimePremier(1);
-        assertEquals("ListeSimple(Noeud(4), Noeud(3), Noeud(2))", listeATester.toString());
+        assertEquals("ListeSimple(Noeud(3), Noeud(2))", listeATester.toString());
         assertEquals(3, listeATester.getSize());
     }
 
@@ -127,8 +126,6 @@ class ListeSimpleTest {
     void supprimePremierEnDerniereElementIntrouvable() {
         listeATester.ajout(1);
         listeATester.ajout(2);
-        listeATester.ajout(3);
-        listeATester.ajout(4);
         listeATester.supprimePremier(8);
         assertEquals("ListeSimple(Noeud(4), Noeud(3), Noeud(2), Noeud(1))", listeATester.toString());
         assertEquals(4, listeATester.getSize());
